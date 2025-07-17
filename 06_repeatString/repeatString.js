aab80bff42ac2ebd76ac52cc042669053c450564
Abraham Trinidad <220383914+PlatinumSuit@users.noreply.github.com>
2025-07-17 09:27:03 -0400

    finish exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    let result = "";
+    for(let i = 0; i < num; i++) {
+        result += string;
+    }
+    return result;
 };
+const number = Math.random() * 1000;
+console.log(repeatString("hey", 3));
+console.log(repeatString("hello", 10));
+console.log(repeatString("hi", 1));
+console.log(repeatString("bye", 0));
+console.log(repeatString("goodbye", -1));
+console.log(repeatString("hey", number));
+console.log(repeatString("", 10));
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
