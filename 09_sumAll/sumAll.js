e79c44b43f8adb5c9f85eb0ce6e4bc0c51120ae8
Abraham Trinidad <220383914+PlatinumSuit@users.noreply.github.com>
2025-07-21 12:52:23 -0400

    finish exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+
+   const start = Math.min(num1, num2);
+   const end = Math.max(num1, num2);
+
+   let sum = 0;
+   for (let i = start; i <= end; i++) {
+    sum += i;
+   }
+   return sum;
 };
-
+console.log(sumAll(2, 4));
+console.log(sumAll(1, 4000));
+console.log(sumAll(-10, 4));
+console.log(sumAll(2.5, 4));
+console.log(sumAll(10, "90"));
+console.log(sumAll(10, [90, 1]));
 // Do not edit below this line
 module.exports = sumAll;
