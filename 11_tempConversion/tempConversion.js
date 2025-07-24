46178594e1304e697f8124148765060bc421bc2a
Abraham Trinidad <220383914+PlatinumSuit@users.noreply.github.com>
2025-07-23 21:24:14 -0400

    Finish exercises

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numF) {
+  return parseFloat(((numF - 32) * 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numC) {
+  return parseFloat(((numC * 9/5) + 32).toFixed(1));
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToCelsius(100));
+console.log(convertToCelsius(-100));
+
+console.log(convertToFahrenheit(0));
+console.log(convertToFahrenheit(73.2));
+console.log(convertToFahrenheit(-10));
+
 
 // Do not edit below this line
 module.exports = {
