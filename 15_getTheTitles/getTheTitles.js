fe301f33f8c816d10bbdff5c2bb69e224e7e60fd
Abraham Trinidad <220383914+PlatinumSuit@users.noreply.github.com>
2025-08-18 08:51:21 -0400

    finish exercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
+const getTheTitles = function(books) {
+    return books.map((obj) => {
+        return obj.title;
+    })
 };
+
+getTheTitles(books)
 
 // Do not edit below this line
 module.exports = getTheTitles;
