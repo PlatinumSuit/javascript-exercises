7f2328206a9fabe123161d93c4a4ef04a2bcaf50
Abraham Trinidad <220383914+PlatinumSuit@users.noreply.github.com>
2025-08-18 10:06:57 -0400

    finish exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+  if (!person.yearOfDeath) {
+    return new Date().getFullYear() - person.yearOfBirth;
+  }
+  return person.yearOfDeath - person.yearOfBirth;
+};
 
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    const oldestAge = getAge(oldest);
+    const personAge = getAge(person);
+    return personAge > oldestAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
